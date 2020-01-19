b2532028b98a1e63e42a6e2905c3c1baa6d10ebb
Flavian <Flavianmohanraj@gmail.com>
2020-01-19 02:06:54 +0000

    updated loan

--- w2/loan.js
+++ w2/loan.js
@@ -1,9 +1,5 @@
 let creditLimit = 100;
-/*
- * Input: number of dollars to loan out
- * Returns: Promise of loan which may or may not fulfill, based on remaining credit. 
- * If creditLimit is less than the requested amount, only the remaining limit is loaned out, otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
- */
+
 const loanOut = function(amount) {
   return new Promise((resolve, reject)=>{
     if(creditLimit <= 0){
